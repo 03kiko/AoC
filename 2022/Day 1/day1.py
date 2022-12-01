b05fc9fa68ae96d68e7761db7e451c280a10d087
03kiko <francisco.miguel.monica@gmail.com>
2022-12-01 22:08:23 +0000

    part1 + part2 of day1 done in Python

--- 2022/Day 1/day1.py
+++ 2022/Day 1/day1.py
@@ -1,13 +1,17 @@
 def most_cal(input):
+    elf_cal=[]
     sum=0
-    max=0
+    elf_counter=0
     for x in input:
         if x!='\n':
             sum+=int(x)
-        elif max<sum:
-            max=sum
+        else: 
+            elf_counter+=1
+            elf_cal+=[[elf_counter,sum]]
             sum=0
-        else:    
-            sum=0
-    return max
+    elf_cal=sorted(elf_cal,key=lambda x:x[1],reverse=True)
+    total_top_3=0
+    for i in range(0,3):
+        total_top_3+=elf_cal[i][1]
+    return total_top_3
 print(most_cal(open('input.txt','r')))
