516ca7028b9b81e7d3288e7308652cb88ebb0a16
03kiko <francisco.miguel.monica@gmail.com>
2022-12-04 11:20:55 +0000

    Added some coments to part1

--- 2022/Day4/day4-part1.py
+++ 2022/Day4/day4-part1.py
@@ -9,7 +9,10 @@ def assignment_pairs(input):
             first_elf.append(i)
         for i in range(int(pairs_limits[2]),int(pairs_limits[3])+1):
             second_elf.append(i)
-        contains=all(id in first_elf for id in second_elf) or all(id in second_elf for id in first_elf)
+        contains=(all(id in first_elf for id in second_elf) or 
+                  all(id in second_elf for id in first_elf))
+        # Checks if all ids of the first elf are fully coitained by the second elf
+        # and vice-versa.
         if contains:
             pairs_fully_contained_in_the_other+=1
     return pairs_fully_contained_in_the_other
